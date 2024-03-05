01091d3df216c8d5c0e4b992d8a10c5960827a6d
Gabriel Godoy <gabyreload@gmail.com>
2024-03-05 16:15:58 -0300

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -16,14 +16,10 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to GSys.</strong>
           </p>
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
@@ -32,15 +28,15 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image
             src="/hero-desktop.png"
-            width={1000}
-            height={760}
+            width={1750}
+            height={909}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
             src="/hero-mobile.png"
-            width={560}
-            height={620}
+            width={468}
+            height={815}
             className="block md:hidden"
             alt="Screenshots of the dashboard project showing mobile version"
           />
