7f07987e21eb30009e2731f278326e9eff7b5024
Gabriel Godoy <gabyreload@gmail.com>
2024-03-28 11:30:47 -0300

    Not Found pages

---
+++ app/dashboard/orders/not-found.tsx
@@ -0,0 +1,5 @@
+import NotFoundPage from '@/app/ui/NotFoundPage';
+
+export default function NotFound() {
+  return <NotFoundPage message="Could not find the requested order." />;
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+import NotFoundPage from '@/app/ui/NotFoundPage';
+
+export default function NotFound() {
+  return <NotFoundPage />;
+}
